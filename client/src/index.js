f5c654884130803451d0d806df776d083942bb47
Cele Gracia <celedgracia@gmail.com>
2017-07-17 00:10:19 -0500

    added react and redux friendly modal component

--- client/src/index.js
+++ client/src/index.js
@@ -13,8 +13,10 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
+export const store = createStoreWithMiddleware(reducers);
+
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <BrowserRouter>
             <div>
                 <Switch>
