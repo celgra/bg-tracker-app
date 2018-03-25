ab76a2f05073cbdf7abe22074cca50dda1ce39e7
Cele Gracia <celedgracia@gmail.com>
2018-03-24 22:25:19 -0500

    ADD missing file renames

---
+++ client/src/reducers/reducer_auth.js
@@ -0,0 +1,22 @@
+//@flow
+import { CHANGE_AUTH, AUTH_ERROR } from './../actions/actions_auth';
+
+type AuthState = { auth: boolean, error: null | {} };
+type AuthAction = { type: String, payload: boolean };
+
+export default function (state: AuthState = { auth: false, error: null }, action: AuthAction): AuthState {
+    switch(action.type) {
+        case CHANGE_AUTH:
+            return changeAuth(state, action);
+        case AUTH_ERROR:
+            return { ...state, error: {} };
+        default:
+            return state;
+    }
+};
+
+const changeAuth = (state: AuthState, action: AuthAction): AuthState => {
+    return { 
+        ...state, auth: action.payload, error: null
+    }; 
+};
